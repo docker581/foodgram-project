23707c66676e115eb700ff7fa2cdff97661b614a
Denis Doktorov <denisd581@gmail.com>
2021-05-12 17:29:31 +0300

    Start auth

--- foodgram_project/urls.py
+++ foodgram_project/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("auth/", include("users.urls")),
+    path("auth/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
-     path('', include('data.urls')),
+    path('', include('data.urls')),
 ]
--- users/forms.py
+++ users/forms.py
@@ -7,4 +7,10 @@ User = get_user_model()
 class CreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ("first_name", "last_name", "username", "email")
+        fields = ("first_name", "username", "email")
+
+    def __init__(self, *args, **kwargs):
+        super(CreationForm, self).__init__(*args, **kwargs)
+        del self.fields['password2']
+        self.fields['password1'].help_text = None
+        self.fields['username'].help_text = None    
