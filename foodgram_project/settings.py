07b033dcb92770af7861d923b3ae93739677faf1
Denis Doktorov <denisd581@gmail.com>
2021-06-03 19:15:20 +0300

    Docker-compose 2.0

--- foodgram_project/settings.py
+++ foodgram_project/settings.py
@@ -120,7 +120,7 @@ LOGOUT_REDIRECT_URL = "index"
 # Static and media
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ['staticfiles']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
