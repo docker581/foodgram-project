858a91afc27ca7c5675e3ac63fac8ebf38b01e44
Denis Doktorov <denisd581@gmail.com>
2021-06-03 17:22:32 +0300

    Continue docker-compose

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,8 @@ WORKDIR /code
 
 COPY requirements.txt .
 
-RUN pip install -r .requirements.txt
+RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
+CMD gunicorn foodgram_project.wsgi:application --bind 0.0.0.0:8000
--- foodgram_project/settings.py
+++ foodgram_project/settings.py
@@ -68,10 +68,6 @@ WSGI_APPLICATION = 'foodgram_project.wsgi.application'
 # Database
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
         'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.postgresql'),
         'NAME': os.environ.get('DB_NAME'),
