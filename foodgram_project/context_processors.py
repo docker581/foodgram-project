975a5bb16446cf38f7f095851fd1352944cd3739
Denis Doktorov <denisd581@gmail.com>
2021-06-12 02:13:19 +0300

    Test project v1.3

--- foodgram_project/context_processors.py
+++ foodgram_project/context_processors.py
@@ -3,5 +3,5 @@ from data.models import Purchase
 
 def purchases_number(request):
     return {
-        'purchases_number': Purchase.objects.count()
+        'purchases_number': Purchase.objects.filter(user=request.user).count()
     }
