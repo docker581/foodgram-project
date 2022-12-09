9a7fd101a68c33f692f0432940c6f166082ad54f
Denis Doktorov <denisd581@gmail.com>
2022-12-09 13:22:58 +0300

    Fix PEP8 error for workflow

--- foodgram_project/context_processors.py
+++ foodgram_project/context_processors.py
@@ -4,5 +4,7 @@ from data.models import Purchase
 def purchases_number(request):
     if request.user:
         return {
-            'purchases_number': Purchase.objects.filter(user=request.user).count()
+            'purchases_number': Purchase.objects.filter(
+                user=request.user
+            ).count()
         }
